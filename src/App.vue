6f986236f314311b69352927584824c64889b3e5
Laur Tõnisson <43137916+lauuur@users.noreply.github.com>
2019-12-16 21:58:08 +0200

    sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <v-app>
-    <Tabs/>
+    <router-view/>
     <notifications group="foo" position="bottom right" max="3" width="65%"/>
   </v-app>
 </template>
 
 <script>
-import Tabs from './components/Tabs';
+// import Tabs from './components/Tabs';
 
 export default {
   name: 'App',
 
   components: {
-    Tabs,
+    // Tabs,
   },
 
   data: () => ({
